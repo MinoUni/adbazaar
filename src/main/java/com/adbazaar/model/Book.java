55fe5811fe5e59bf12da55c2e0d755693a18e99b
MinoUni <maks.reva.ua@gmail.com>
2023-10-16 12:30:27 +0300

    Refactor id generation to fix pk violation

--- src/main/java/com/adbazaar/model/Book.java
+++ src/main/java/com/adbazaar/model/Book.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
@@ -37,7 +38,7 @@ import java.util.Set;
 public class Book {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
     private Long id;
 
